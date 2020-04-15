0d281320b22e7488263ac00c633427aebb59768c
emenaTechtonic <menaelena.littleton@gmail.com>
2020-04-15 14:10:12 -0600

    Solved

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main()  {
-	
+import "fmt"
+
+func main() {
+	s := "DDUUDDUDUUUD"
+	sl := 0
+	valley := 0
+	prevsl := 0
+
+	for i := 0; i < len(s); i++ {
+		prevsl = sl
+
+		if s[i] == 'D' {
+			sl--
+		} else {
+			sl++
+		}
+		if (sl == -1) && (prevsl == 0) {
+			valley++
+		}
+	}
+
+	fmt.Println(valley)
 }
